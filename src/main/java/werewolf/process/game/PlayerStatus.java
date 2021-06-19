5169db950d0c3ca7fc3c139167defa865f688762
al19012 <al19012@shibaura-it.ac.jp>
2021-06-19 17:02:17 +0900

    だす
    だす

--- src/main/java/werewolf/process/game/PlayerStatus.java
+++ src/main/java/werewolf/process/game/PlayerStatus.java
@@ -1,9 +1,9 @@
 //PlayersStatusに使う構造体
-public class PlayerStatus{
+
+public class PlayerStatus {
     public boolean alive;
     public String role;
     public PlayerStatus() {
-	bool alive=true;
+	bool alive = true;
     }
 }
-	
--- src/main/java/werewolf/process/game/VotingAction.java
+++ src/main/java/werewolf/process/game/VotingAction.java
@@ -1,46 +1,58 @@
 import java.util.*;
 
-public class VotingAction{
-    Map<UUID,UUID>votesData=new HashMap<UUID,UUID>();
+//ゲームのロジックを持ち実行する。
+
+public class VotingAction {
+    Map <UUID,UUID> votesData = new HashMap <UUID,UUID>();
     PlayersStatus playersStatus;
     UUID votingResult = null;
-    static void VotingAction(PlayerStatus playerStatus){
+
+    //ゲームに参加するプレイヤー毎のUUIDと対応する役職と生死の状態を持たせる。
+
+    static void VotingAction ( PlayerStatus playerStatus) {
 	playerStatus = votesData;
 	    }
-    static void vote(UUID userUUID, UUID targetUserUUID){
-	votesData.put(userUUID,targetUserUUID);
+
+    //投票者と投票者の投票先の情報を追加する。
+
+    static void vote ( UUID userUUID, UUID targetUserUUID) {
+	votesData.put( userUUID,targetUserUUID);
     }
-    boolean finishVote() {
-	boolean isTied=false;
+    
+    //投票結果を確定させる。同数なものがあったら変数を代入して返す
+
+    boolean finishVote () {
+	boolean isTied = false;
 	int max = 0;
 	UUID result = null;
-	for(String s ; targetUUID){
+	for ( String s ; targetUUID) {
 	    int v;
-	    if(m.containsKey(s)){
-		v=votesData.get(s)+1;
-	    }else{
-		v=1;
-	    }
-	    votesData.put(s, v);
-	}
-	Map<UUID,Integer> votedCount = new HashMap<UUID,Integer>();
-	for(Entry<UUID, UUID> entry : votesData.entrySet()){
-	    if(votedCount.get(entry.getValue()) == null){
-		votedCount.put(entry.getValue(), 1);
+	    if ( m.containsKey( s)) {
+		v = votesData.get( s) + 1;
 	    } else {
-		votedCount.replace(entry.getValue(), votedCount.get(entry.getValue())+1);
+		v = 1;
+	    }
+	    votesData.put( s, v);
+	}
+	Map <UUID,Integer> votedCount = new HashMap <UUID,Integer>();
+	for (Entry <UUID, UUID> entry : votesData.entrySet()) {
+	    if ( votedCount.get( entry.getValue()) == null){
+		votedCount.put( entry.getValue(), 1);
+	    } else {
+		votedCount.replace( entry.getValue(), 
+				    votedCount.get(entry.getValue()) + 1);
 	    }
 	}
-	for(Entry<UUID,Integer> entry2 : votedCount){
-	    if(entry2.getValue() > max){
+	for ( Entry <UUID,Integer> entry2 : votedCount) {
+	    if ( entry2.getValue() > max) {
 		isTied = false;
 		max = entry2.getValue();
 		result = entry2.getKey();
-	    } else if(entry2.getValue() == max){
+	    } else if ( entry2.getValue() == max) {
 		isTied = true;
 	    }
 	} 
-	if(isTied){
+	if ( isTied) {
 	    votingResult = null;
 	    return false;
 	} else {
@@ -48,11 +60,25 @@ public class VotingAction{
 	    return true;
 	}
     }
-    UUID getVotingResult(){
+    
+    //投票されたプレイヤーを返す。複数いたらnullを返す。
+
+    UUID getVotingResult() {
 	return votingResult;
     }
-    vote startNewVoting(){
-	Map<UUID,UUID> voteData=new HashMap<UUID,UUID>();
+
+    //投票されたプレイヤーが複数の時ランダムに一人選ぶ。
+
+    UUID determineRandomly() {
+	if ( votingResult == null) {
+	    int index = new Ramdom().nextInt(votingResult.size());
+	    UUID votingResult = votingResult.get(index);
+	    return votingResult;
+	}
+    }
+    
+    vote startNewVoting() {
+	Map <UUID,UUID> voteData = new HashMap <UUID,UUID>();
     }
 }
 	    
